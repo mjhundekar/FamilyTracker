8265e82855aae266eb78fc37cf81fa98a61845c0
umanggala <umanggala9@gmail.com>
2016-10-20 21:15:55 -0700

    Login Activity working

--- mobile/src/main/java/com/example/mjhundekar/family_tracker/LoginActivity.java
+++ mobile/src/main/java/com/example/mjhundekar/family_tracker/LoginActivity.java
@@ -6,13 +6,33 @@ package com.example.mjhundekar.family_tracker;
 
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public Button but1;
+
+    Context mcontext;
     @VisibleForTesting
     public ProgressDialog mProgressDialog;
+
+    public void init(){
+        but1 = (Button)findViewById(R.id.sign_in_button);
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, EmailPassword.class);
+                startActivity(intent);
+            }
+        });
+    }
 
     public void showProgressDialog() {
         if (mProgressDialog == null) {
@@ -34,6 +54,14 @@ public class LoginActivity extends AppCompatActivity {
     public void onStop() {
         super.onStop();
         hideProgressDialog();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        mcontext = this;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
     }
 
 }
